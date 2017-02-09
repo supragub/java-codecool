6926231cf5ea08b11f2ada7ded65b6f65c2378b1
Gábor Varga <gabor.varga@suprasoft.hu>
2017-02-09 15:51:37 +0100

    Hello World

---
+++ 001-HelloWorld/src/hu/supraweb/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package hu.supraweb.helloworld;
+
+/**
+ * @author VarGabor
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
