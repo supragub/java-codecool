add433c89f5abb61530b1412e3dd6b63f46b018e
Gábor Varga <gabor.varga@suprasoft.hu>
2017-02-15 16:00:29 +0100

    3. Task done - created FeltartosTej, TartosTej class extends Tej

---
+++ 011-Shop/src/hu/suprasoft/bolt/FeltartosTej.java
@@ -0,0 +1,15 @@
+package hu.suprasoft.bolt;
+
+import java.util.Date;
+
+/**
+ * @author VarGabor
+ *
+ */
+
+public class FeltartosTej extends Tej {
+
+	public FeltartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+		super(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+	}
+}
---
+++ 011-Shop/src/hu/suprasoft/bolt/TartosTej.java
@@ -0,0 +1,15 @@
+package hu.suprasoft.bolt;
+
+import java.util.Date;
+
+/**
+ * @author VarGabor
+ *
+ */
+
+public class TartosTej extends Tej {
+
+	public TartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+		super(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+	}
+}
